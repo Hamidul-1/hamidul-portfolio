ff983c21e72c2891708f2349b9ea490e12fd14c9
Hamidul-1 <hamidul786cse@gmail.com>
2022-06-09 18:34:55 +0600

    first commit

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,29 +1,34 @@
+import hero from '../assets/svg/achievements/Hero.png';
+import react from '../assets/svg/achievements/react.png';
+import Javascript from '../assets/svg/achievements/Javascript.png';
+
+
 export const achievementData = {
     bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
+            title : 'Complete Web Development',
+            details : 'Personalized, fun, and interactive learning process. Enjoy our gamified courses to master python, Web development, cyber security, C, C++ course and more.',
+            date : 'Still Now',
+            field : 'Web Development',
+            image : hero
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'React.js assessment',
+            details : 'JavaScript assessment',
+            date : 'June 8, 2022',
+            field : 'React',
+            image : react
         },
         {
             id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'JavaScript assessment',
+            details : 'JavaScript assessment',
+            date : 'June 7, 2022',
+            field : 'JavaScript',
+            image : Javascript
         }
 
     ]
